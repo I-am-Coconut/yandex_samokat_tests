c376f18d41b67ec38a263910d27f19ca2b1c5324
Aleksandr <coconutpwnz@yandex.ru>
2025-12-10 05:58:51 +0300

    order_body

---
+++ data.py
@@ -0,0 +1,10 @@
+
+order_body = {
+    "firstName": "Coconut",
+    "lastName": "Кокос",
+    "address": "Земля, 2я пальма.",
+    "metroStation": 2,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2025-12-12",
+}
